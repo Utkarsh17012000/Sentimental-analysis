cdbbe41bf8ec30ea647d24c8a0eeb94ec421a2ee
Utkarsh17012000 <utkarshtanwar007@gmail.com>
2020-06-25 23:18:58 +0530

    Identation and Spacing spacing and indentation is enhanced script is working fine!

--- Logistic_model.py
+++ Logistic_model.py
@@ -23,6 +23,8 @@ reviews_test_clean = cleaning_func(reviews_test)
 
 print("successful!!!")
 
+print("\n\n")
+
 cv = CountVectorizer(binary=True)#,stop_words='english', ngram_range=(2,2) for only bigrams )
 cv.fit(reviews_train_clean)
 
@@ -40,22 +42,24 @@ for c in [0.01,0.05,0.25,0.5,1]:
 	lr.fit(training_data,training_values)
 	print("Accuracy for C: {}, {}".format(c,accuracy_score(testing_values,lr.predict(testing_data))))
 '''
-#C=0.05 come sout to be the best fit
+#C=0.05 comes out to be the best fit
 
 #finally train the model at penality of 0.05 on X_test
 try:
 	final_model = LogisticRegression(C=0.05)
 	final_model.fit(X,target)
 	predictions = final_model.predict(X_test)
-	print("Accuracy is {}".format(accuracy_score(target,predictions)))
+	print("Accuracy(C=0.05): {}".format(accuracy_score(target,predictions)))
 except:
 	print("Error Found!")
+
+print("\n\n")
 
 #Sanity check
 '''
 print(len(final_model.coef_[0])) 
 print(len(cv.get_feature_names())) #sparse matrix columns actually
-'''
+
 feature_to_coef = {
 	word:coef for word,coef in zip(
 		cv.get_feature_names(), final_model.coef_[0]	
@@ -74,7 +78,7 @@ for best_negative in sorted(
 	feature_to_coef.items(),
 	key = lambda x:x[1])[:5]:
 	print(best_negative)
-
+'''
 #-------------------------------------
 
 #Improving the model
@@ -97,7 +101,9 @@ no_stop_words = remove_stop_words(reviews_train_clean)
 
 X = cv.transform(no_stop_words)
 final_model.fit(X,target)
-print("Accuracy(C=0.05) with No Stop Words is: {}\n".format(accuracy_score(target,final_model.predict(X_test)))
+print("Accuracy(C=0.05) with No Stop Words is: {}\n".format(accuracy_score(target,final_model.predict(X_test))))
+
+print("\n\n")
 
 #Normalization(convert all the diff. verb forms of a word into a single word)
 
@@ -115,8 +121,9 @@ stemmed_reviews = to_stem_reviews(reviews_train_clean)
 
 X = cv.transform(stemmed_reviews)
 final_model.fit(X,target)
-print("Accuracy(C=0.05) with Stemmed Reviews is: {}\n".format(accuracy_score(target,final_model.predict(X_test)))
+print("Accuracy(C=0.05) with Stemmed Reviews is: {}\n".format(accuracy_score(target,final_model.predict(X_test))))
 
+print("\n\n")
 
 def to_lemma_reviews(corpus):
 	from nltk.stem import WordNetLemmatizer
@@ -128,7 +135,9 @@ lemmatized_reviews = to_lemma_reviews(reviews_train_clean)
 
 X = cv.transform(lemmatized_reviews)
 final_model.fit(X,target)
-print("Accuracy(C=0.05) with Lemmatized Reviews is: {}\n".format(accuracy_score(target,final_model.predict(X_test)))
+print("Accuracy(C=0.05) with Lemmatized Reviews is: {}\n".format(accuracy_score(target,final_model.predict(X_test))))
+
+print("\n\n")
 
 #ngrams can be used within Vectorizer defination with following defination
 # ngram_cv = CountVectorizer(binary=True, ngrams=(1,2)|(2,2)) 
@@ -137,17 +146,19 @@ print("Accuracy(C=0.05) with Lemmatized Reviews is: {}\n".format(accuracy_score(
 #Instead of using single words as colums to form a sparse matrix, we can count words used in a reviews and word count(highest|lowest) can help us understand review better
 
 wc_cv = CountVectorizer(binary=False)
-wc.fit(reviews_train_clean)
+wc_cv.fit(reviews_train_clean)
 X = wc_cv.transform(reviews_train_clean)
 X_test = wc_cv.transform(reviews_train_clean)
 
 final_model.fit(X,target)
 print("Accuracy(C=0.05) using Wordcount: {}".format(accuracy_score(target,final_model.predict(X_test))))
 
+print("\n\n")
+
 #Tf-Idf(Term frequency-inverse document frequency)
 #it defines number of times a word appear in a document relative to number of documents it's present in
 
-from sklearn.feature_extraction.test import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 tf_vec = TfidfVectorizer()
 tf_vec.fit(reviews_train_clean)
 X = tf_vec.transform(reviews_train_clean)
