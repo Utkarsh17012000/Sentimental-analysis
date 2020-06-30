481ecfc1b8f1e614cd460421ed700d9746d8d07b
Utkarsh17012000 <utkarshtanwar007@gmail.com>
2020-06-30 22:49:58 +0530

    Listed Accuracy for various improvements lemmatization is not working script is working fine!

--- Logistic_model.py
+++ Logistic_model.py
@@ -22,7 +22,6 @@ reviews_train_clean = cleaning_func(reviews_train)
 reviews_test_clean = cleaning_func(reviews_test)
 
 print("successful!!!")
-
 print("\n")
 
 cv = CountVectorizer(binary=True)#,stop_words='english', ngram_range=(2,2) for only bigrams )
@@ -34,9 +33,8 @@ X_test = cv.transform(reviews_test_clean)
 
 #building a classifier
 target = [1 if i<12500 else 0 for i in range(25000)]
-
-training_data, testing_data, training_values, testing_values = train_test_split(X, target, train_size = 0.75)
 '''
+training_data, testing_data, training_values, testing_values = train_test_split(X, target, train_size = 0.75)
 for c in [0.01,0.05,0.25,0.5,1]:
 	lr = LogisticRegression(C=c)
 	lr.fit(training_data,training_values)
@@ -45,15 +43,12 @@ for c in [0.01,0.05,0.25,0.5,1]:
 #C=0.05 comes out to be the best fit
 
 #finally train the model at penality of 0.05 on X_test
-try:
-	final_model = LogisticRegression(C=0.05)
-	final_model.fit(X,target)
-	predictions = final_model.predict(X_test)
-	print("Accuracy(C=0.05): {}".format(accuracy_score(target,predictions)))
-except:
-	print("Error Found!")
-
-print("\n\n")
+final_model = LogisticRegression(C=0.05)
+final_model.fit(X,target)
+predictions = final_model.predict(X_test)
+print("Accuracy(C=0.05): {}".format(accuracy_score(target,predictions)))
+#accuracy=0.88156
+print("\n")
 
 #Sanity check
 '''
@@ -103,8 +98,8 @@ no_stop_words_test = remove_stop_words(reviews_test_clean)
 cv.fit(no_stop_words_train)
 no_stop_X = cv.transform(no_stop_words_train)
 no_stop_X_test = cv.transform(no_stop_words_test)
-#testing best value for C
 '''
+#testing best value for C
 training_data,testing_data,training_values,testing_values = train_test_split(no_stop_X,target,train_size = 0.75)
 for c in [0.01,0.05,0.25,0.5,1]:
 	lr = LogisticRegression(C=c)
@@ -112,9 +107,10 @@ for c in [0.01,0.05,0.25,0.5,1]:
 	print("Accuracy(No stop words reviews) for C: {}, {}".format(c,accuracy_score(testing_values,lr.predict(testing_data))))
 #best C comes out to be 0.05
 '''
+final_model = LogisticRegression(C=0.05)
 final_model.fit(no_stop_X,target)
 print("Accuracy(C=0.05) with No Stop Words is: {}\n".format(accuracy_score(target,final_model.predict(no_stop_X_test))))
-
+#accuracy=0.8798
 print("\n")
 
 #Normalization(convert all the diff. verb forms of a word into a single word)
@@ -144,9 +140,10 @@ for c in [0.01,0.05,0.25,0.5,1]:
 	print("Accuracy(stemmed reviews) for C: {}, {}".format(c,accuracy_score(testing_values,lr.predict(testing_data))))
 #best C comes out to be 0.05
 '''
+final_model = LogisticRegression(C=0.05)
 final_model.fit(stem_X,target)
 print("Accuracy(C=0.05) with Stemmed Reviews is: {}\n".format(accuracy_score(target,final_model.predict(stem_X_test))))
-
+#accuracy=0.93748
 print("\n")
 '''
 def to_lemma_reviews(corpus):
@@ -187,11 +184,13 @@ training_data,testing_data,training_values,testing_values = train_test_split(n_g
 for c in [0.01,0.05,0.25,0.5,1]:
 	lr = LogisticRegression(C=c)
 	lr.fit(training_data,training_values)
-	print("Accuracy(Wordcount Vectorizer) for C: {}, {}".format(c,accuracy_score(testing_values,lr.predict(testing_data))))
-#best C comes out to be 0.05
+	print("Accuracy(ngram Vectorizer) for C: {}, {}".format(c,accuracy_score(testing_values,lr.predict(testing_data))))
+#best C comes out to be 0.25
 '''
+final_model = LogisticRegression(C=0.25)
 final_model.fit(n_gram_X,target)
 print("Accuracy(C=0.05) with n_gram Vectorizer: {}".format(accuracy_score(target,final_model.predict(n_gram_X_test))))
+#accuracy=0.8994
 print('\n')
 
 #Instead of using single words as colums to form a sparse matrix, we can count words used in a reviews and word count(highest|lowest) can help us understand review better
@@ -199,9 +198,8 @@ wc_cv = CountVectorizer(binary=False)
 wc_cv.fit(reviews_train_clean)
 wc_X = wc_cv.transform(reviews_train_clean)
 wc_X_test = wc_cv.transform(reviews_train_clean)
-
-#testing various values of C for best fit
 '''
+#testing various values of C for best fit
 training_data,testing_data,training_values,testing_values = train_test_split(wc_X,target,train_size = 0.75)
 for c in [0.01,0.05,0.25,0.5,1]:
 	lr = LogisticRegression(C=c)
@@ -209,9 +207,10 @@ for c in [0.01,0.05,0.25,0.5,1]:
 	print("Accuracy(Wordcount Vectorizer) for C: {}, {}".format(c,accuracy_score(testing_values,lr.predict(testing_data))))
 #best C comes out to be 0.05
 '''
+final_model = LogisticRegression(C=0.05)
 final_model.fit(wc_X,target)
 print("Accuracy(C=0.05) using Wordcount: {}".format(accuracy_score(target,final_model.predict(wc_X_test))))
-
+#accuracy=0.95896
 print("\n")
 
 #Tf-Idf(Term frequency-inverse document frequency)
@@ -223,19 +222,19 @@ tf_vec.fit(reviews_train_clean)
 
 tf_X = tf_vec.transform(reviews_train_clean)
 tf_X_test = tf_vec.transform(reviews_test_clean)
-
-#testing various valuyes of C
 '''
+#testing various valuyes of C
 training_data,testing_data,training_values,testing_values = train_test_split(tf_X,target,train_size = 0.75)
 for c in [0.01,0.05,0.25,0.5,1]:
 	lr = LogisticRegression(C=c)
 	lr.fit(training_data,training_values)
-	print("Accuracy(Wordcount Vectorizer) for C: {}, {}".format(c,accuracy_score(testing_values,lr.predict(testing_data))))
-#best C comes out to be 0.05
+	print("Accuracy(tf-idf Vectorizer) for C: {}, {}".format(c,accuracy_score(testing_values,lr.predict(testing_data))))
+#best C comes out to be 1
 '''
+final_model = LogisticRegression(C=1)
 final_model.fit(tf_X,target)
 print("Accuracy(C=0.05) using Tf-Idf: {}".format(accuracy_score(target,final_model.predict(X_test))))
-
+#accuracy=0.86204
 #-------------------------------------------------------------------------------
 
 
